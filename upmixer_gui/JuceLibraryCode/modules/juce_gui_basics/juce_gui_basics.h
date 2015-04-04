74860565eecba9448948fc16b7326b4993c76d9c
Brian Leu <brian.d.leu@gmail.com>
2015-04-04 12:40:51 -0400

    Fixing JUCE paths, hopefully

---
+++ upmixer_gui/JuceLibraryCode/modules/juce_gui_basics/juce_gui_basics.h
@@ -0,0 +1,11 @@
+// This is an auto-generated file to redirect any included
+// module headers to the correct external folder.
+
+#if defined (JUCER_XCODE_MAC_F6D2F4CF)
+ #include "../../../../../Downloads/JUCE-OSX/modules/juce_gui_basics/juce_gui_basics.h"
+#elif defined (JUCER_LINUX_MAKE_7346DA2A)
+ #include "../../../../../../JUCE/modules/juce_gui_basics/juce_gui_basics.h"
+#else
+ #error "This file is designed to be used in an Introjucer-generated project!"
+#endif
+
